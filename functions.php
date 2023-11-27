7fde21b35495c281f8200f722cf5b44262eea504
Divya21146 <113895154+Divya21146@users.noreply.github.com>
2023-11-27 18:06:20 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php 
+
+function custom_table() {
+    global $wpdb;
+ 
+ $table_name = $wpdb->prefix."user_data"; //change the "user_data" to your table name
+
+ $charset_collate = $wpdb->get_charset_collate();
+
+ $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+    id mediumint(9) NOT NULL AUTO_INCREMENT,
+    email varchar(100) NOT NULL,
+    user_name varchar(100) NOT NULL,
+    created_date datetime NOT NULL,
+    PRIMARY KEY (id)
+)";
+
+ require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+ dbDelta($sql);
+
+}    
+ 
+add_action('init', 'custom_table');
